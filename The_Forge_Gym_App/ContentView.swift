0a268faa0198e593512163402b13189247231d8e
Ben Foster <benfost018@gmail.com>
2026-07-17 14:24:51 +0100

    Initial Commit

---
+++ The_Forge_Gym_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The_Forge_Gym_App
+//
+//  Created by Ben Foster on 17/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
