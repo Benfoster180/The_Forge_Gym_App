5eb6715b139ad047ecb2937887fd6b7bc162001c
Ben Foster <benfost018@gmail.com>
2026-07-18 17:34:44 +0100

    Awful PR control from myself adding parking. access scanables finished the home screen created blank pages for some buttons

---
+++ The_Forge_Gym_App/Views/Components/Buttons/ForgeParkingScan.swift
@@ -0,0 +1,45 @@
+//
+//  ForgeParking.swift
+//  The_Forge_Gym_App
+//
+//  Created by Ben Foster on 18/07/2026.
+//
+
+import SwiftUI
+
+struct ForgeParkingScan: View {
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.white.opacity(0.05))
+                .overlay {
+                    Rectangle()
+                        .stroke(Color.white.opacity(0.15), lineWidth: 1)
+                    
+                    VStack {
+                        Image(systemName: "figure.cooldown")
+                            .foregroundColor(Color("ForgeYellow"))
+                            .font(.system(size: 35))
+                            .padding()
+                        
+                        Text("Book A Class!")
+                            .foregroundColor(Color("ForgeYellow"))
+                            .font(.system(size: 16, weight: .bold))
+                            .tracking(1)
+                    }
+                }
+        }
+        .frame(height: 160)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color("ForgeBlack")
+            .ignoresSafeArea()
+
+        ForgeParkingScan()
+            .frame(width: 200, height: 160)
+    }
+}
